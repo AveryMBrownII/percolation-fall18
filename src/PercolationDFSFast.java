8e9deba691521fb58c96f90de1aacdbe5610fca4
Avery Brown <averymbrown22@gmail.com>
2018-11-29 18:41:06 -0500

    update

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -8,9 +8,17 @@ public class PercolationDFSFast extends PercolationDFS {
 	@Override
 	protected void updateOnOpen(int row, int col) {
 		if (row == 0) dfs(0,col);
-		else if (isFull(row-1, col) == true) dfs(row,col);
-		else if (isFull(row, col-1) == true) dfs(row,col);
-		else if (isFull(row, col+1) == true) dfs(row,col);
-		else if (isFull(row+1, col) == true) dfs(row,col);
+		else if (inBounds(row-1,col)) {
+			if (isFull(row-1, col)) dfs(row,col);
+		}
+		else if (inBounds(row,col-1)) {
+			if (isFull(row, col-1)) dfs(row,col);
+		}
+		else if (inBounds(row,col+1)) {
+			if (isFull(row, col+1)) dfs(row,col);
+		}
+		else if (inBounds(row+1,col)) {
+			if (isFull(row+1, col)) dfs(row,col);
+		}
 	} 
 }
