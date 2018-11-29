5e914bb91ad97331db17c234428eb07e449ee103
Avery Brown <averymbrown22@gmail.com>
2018-11-29 18:57:18 -0500

    baby

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -8,7 +8,18 @@ public class PercolationDFSFast extends PercolationDFS {
 	@Override
 	protected void updateOnOpen(int row, int col) {
 		if (row == 0) dfs(0,col);
-		if (inBounds(row-1,col)) {
+		else {
+			int[]a = {-1,0,0,1};
+	        int[]b = {0,-1,1,0};
+	        for (int x = 0; x < 4; x++) {
+	        	if (inBounds(row+a[x], col+b[x])) {
+	        		dfs(row,col);
+	        		break;
+	        	}
+	        }
+		}
+			
+		/*if (inBounds(row-1,col)) {
 			if (isFull(row-1, col)) dfs(row,col);
 		}
 		if (inBounds(row,col-1)) {
@@ -19,6 +30,6 @@ public class PercolationDFSFast extends PercolationDFS {
 		}
 		if (inBounds(row+1,col)) {
 			if (isFull(row+1, col)) dfs(row,col);
-		}
+		}*/
 	} 
 }
